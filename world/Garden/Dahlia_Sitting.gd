0708a822a794551e26e383def0260ef8652aaeba
Kevin Liang <kevinpliang@gmail.com>
2021-05-28 11:26:48 -0400

    dialogic onboarding

---
+++ world/Garden/Dahlia_Sitting.gd
@@ -0,0 +1,27 @@
+extends AnimatedSprite
+
+var can_talk = false
+var dialogue1
+var dialogue_num = 1
+
+func _ready():
+	dialogue1 = Dialogic.start("dahlia_sitting_1")
+	dialogue1.connect("timeline_end", self, "on_dialogue_end")
+	
+func _process(delta):
+	if Input.is_action_pressed("a"):
+		if can_talk:
+			match dialogue_num:
+				1:
+					add_child(dialogue1)
+				2:
+					pass			
+	
+func _on_dialogue_range_area_entered(area):
+	can_talk = true
+
+func _on_dialogue_range_area_exited(area):
+	can_talk = false
+
+func on_dialogue_end():
+	dialogue_num +=1;
