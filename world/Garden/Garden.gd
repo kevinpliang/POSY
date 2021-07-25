16f833bd51b28d707dac3c27b475d469fe6f26d8
Kevin Liang <kevinpliang@gmail.com>
2021-07-25 00:27:26 -0400

    name entry polish

--- world/Garden/Garden.gd
+++ world/Garden/Garden.gd
@@ -7,6 +7,9 @@ var dahliaInstance
 enum STATES {IDLE, CS0, CS1, CS2}
 var curr_state
 
+# makes it so accepting name prompt doesn't auto fill "p" in text box
+var firstP = false
+
 func _ready():
 	curr_state = STATES.CS0
 	dahliaInstance = Global.instance_node_at(Dahlia, Vector2(700,400), self)
@@ -21,6 +24,13 @@ func _process(delta):
 	if (curr_state == STATES.CS0 && dahliaInstance.global_position.x - Global.player.global_position.x < 100):
 		dahliaInstance.vel = Vector2.ZERO
 		cutscene1()
+
+# makes it so accepting name prompt doesn't auto fill "p" in text box
+func _input(event):
+	if (!firstP and $NameEntry.has_focus() and  event.is_action_pressed("a")):
+		firstP = false
+		yield(get_tree().create_timer(0.001), "timeout")
+		$NameEntry.clear()		
 		
 func cutscene1():
 	curr_state = STATES.CS1
@@ -33,13 +43,23 @@ func showNameEntry(timeline_end):
 	$NameEntry.grab_focus()
 	
 func _on_NameEntry_text_entered(new_text):
-	Global.playername = new_text
-	Dialogic.set_variable("playername", new_text)
-	$NameEntry.visible = false
-	$NameEntry.release_focus()
-	var Garden2 = Dialogic.start('Garden2', false)
-	add_child(Garden2)
-	Garden2.connect("timeline_end", self, "cutscene2")
+	if (isValidName(new_text)):
+		Global.playername = new_text
+		Dialogic.set_variable("playername", new_text)
+		$NameEntry.visible = false
+		$NameEntry.release_focus()
+		var Garden2 = Dialogic.start('Garden2', false)
+		add_child(Garden2)
+		Garden2.connect("timeline_end", self, "cutscene2")
+	else:
+		$NameEntry.clear()
+		$NameEntry.placeholder_text = "Invalid Name"
+
+# Checks if the submitted player name is valid
+func isValidName(name) -> bool:
+	if (name.length() < 2):
+		return false
+	return true
 
 func cutscene2(timeline_end):
 	curr_state = STATES.CS2
