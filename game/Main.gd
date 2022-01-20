a85a71f22f7484ba5075a1bd97164a77e134bf98
Kevin <kevinpliang@gmail.com>
2022-01-20 12:44:09 -0500

    straggler commit + delete imports

--- game/Main.gd
+++ game/Main.gd
@@ -24,19 +24,22 @@ func _process(_delta):
 func startGame():
 	toggleMainMenu()
 	if Global.loadGame():
-		movePlayer(null, sceneOf[Global.playerlocation])
+		movePlayer(Global.playerlocation)
 	else:
-		movePlayer(null, sceneOf[Global.LOCATIONS.GARDEN])
+		movePlayer(Global.LOCATIONS.GARDEN)
 		
-func movePlayer(from, to):
-	if (Global.player):
-		Global.player.queue_free()
-	if (Global.playerlocation):
-		Global.playerlocation.queue_free()
-	Global.playerlocation = to
-	var new_location_instance = Global.instance_node(to, $Game)
+func movePlayer(to):
+	removeAllChildren($Game)
+	Global.playerlocation = sceneOf[to]
+	var new_location_instance = Global.instance_node(sceneOf[to], $Game)
 	Global.player = Global.instance_node_at(Zee, new_location_instance.spawn, $Game)
 	Global.instance_node_at(HealthBar, Vector2(125,125), $UI)
+
+# Helper methods
+func removeAllChildren(node):
+	for child in node.get_children():
+		node.remove_child(child)
+		child.queue_free()
 
 # Menu Stuff
 func _input(event):
---
+++ world/Garden/Hole.gd
@@ -0,0 +1,16 @@
+extends Sprite
+
+var active = false
+
+func _input(event):
+	if (event.is_action_pressed("a") and active):
+		Global.main.movePlayer(Global.LOCATIONS.GARDEN)
+
+func _on_Area2D_area_entered(area):
+	if (area.is_in_group("player")):
+		active = true		
+
+
+func _on_Area2D_area_exited(area):
+	if (area.is_in_group("player")):
+		active = false
