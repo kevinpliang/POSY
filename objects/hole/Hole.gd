31c679fc40128980b60ff2b0f3eb25e07a89952d
Kevin <kevinpliang@gmail.com>
2022-02-10 23:53:04 -0500

    Generalized hole

--- objects/hole/Hole.gd
+++ objects/hole/Hole.gd
@@ -1,10 +1,13 @@
 extends Sprite
 
+class_name Hole
+
+var dest
 var active = false
 
 func _input(event):
 	if (event.is_action_pressed("a") and active):
-		Global.main.movePlayer(Global.LOCATIONS.GARDEN)
+		Global.main.movePlayer(dest)
 
 func _on_Area2D_area_entered(area):
 	if (area.is_in_group("player")):
@@ -13,3 +16,6 @@ func _on_Area2D_area_entered(area):
 func _on_Area2D_area_exited(area):
 	if (area.is_in_group("player")):
 		active = false
+
+func _init(d):
+	dest = d
